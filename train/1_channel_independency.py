3b70d6593620f8056c9eb9cd89b3ec800e377969
dlcjfgmlnasa28 <dlcjfgmlnasa28@gmail.com>
2026-03-26 12:38:53 +0900

    Revert find_unused_parameters to True for DDP compatibility
    Parameters 149/150 are unused in some Phase 1 configurations,
    causing RuntimeError with find_unused_parameters=False.

--- train/1_channel_independency.py
+++ train/1_channel_independency.py
@@ -308,7 +308,7 @@ def main():
     model.to(device)
 
     if use_ddp:
-        model = DDP(model, device_ids=[local_rank], find_unused_parameters=False)
+        model = DDP(model, device_ids=[local_rank], find_unused_parameters=True)
 
     if rank0:
         raw_model = model.module if use_ddp else model
